fb41b9d9bf74867ef6ad8be121ebeb8260378db4
Joshua Miller <joshua_m1989@yahoo.com>
2014-10-21 20:51:19 -0400

    Update Program.cs

--- DisableSpells/Program.cs
+++ DisableSpells/Program.cs
@@ -1,154 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Runtime.InteropServices;
-using LeagueSharp.Common;
+using System.Net.Sockets;
 using LeagueSharp;
-
-namespace DisableSpells
+using LeagueSharp.Common;
+using LX_Orbwalker;
+using SharpDX;
+ 
+namespace Autocombo
 {
-    internal class Program
+ 
+    internal class Autocombo2
     {
-        public struct SpellStruct
-        {
-            public string ChampionName;
-            public SpellSlot AvailableSpell;
-
-        }
-        public static List<SpellStruct> Spells = new List<SpellStruct>();
+        public static Obj_AI_Hero Player;
+        public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
         public static Menu Config;
-
+ 
         private static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
         }
-
-        private static void Game_OnGameLoad(EventArgs args)
-        {
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Rumble",
-                AvailableSpell = SpellSlot.W
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Sion",
-                AvailableSpell = SpellSlot.W
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "TwistedFate",
-                AvailableSpell = SpellSlot.W
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Jax",
-                AvailableSpell = SpellSlot.E
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "MasterYi",
-                AvailableSpell = SpellSlot.R
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Annie",
-                AvailableSpell = SpellSlot.E
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Singed",
-                AvailableSpell = SpellSlot.R
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Vayne",
-                AvailableSpell = SpellSlot.R
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Tryndamere",
-                AvailableSpell = SpellSlot.R
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Teemo",
-                AvailableSpell = SpellSlot.W
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Blitzcrank",
-                AvailableSpell = SpellSlot.W
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Ashe",
-                AvailableSpell = SpellSlot.Q
-            });
-
-            Spells.Add(new SpellStruct
-            {
-                ChampionName = "Zilean",
-                AvailableSpell = SpellSlot.W
-            });
-
-            /*
-             * Ashe:
-             * -> Fiora: OnAttack: Instant ultimate / no duration limit / less damage / can be attacked
-             * -> Twitch: OnAttack: Cast's W without CD except of AA
-             * -> TwistedFate: OnAttack: Always shoots with red card
-             * -> Ezreal: OnAttack: E particle, ways less damage, ways less attackspeed
-             * -> Lucian: OnAttack: R particle, goes throguh enemys, ways less damage, ways less attackspeed
-             * -> Brand: OnAttack: Ultimate
-             * -> Pantheon: Weird shit.
-             * -> Gragas: OnAttack: Ultimate with a cd of 10-15sec
-             * -> Varus: Uses the area Damage on attack
-             * -> Jax: Possible to stun everyone
-             * -> Lulu: OnAttack: Lulu AA becomes her Q and Pix also CS
-             */
-
-            Config = new Menu("Exploit", "Exploit", true);
-            Config.AddSubMenu(new Menu("Disable", "Disable"));
-            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => !hero.IsMe))
-            {
-                Config.SubMenu("Disable")
-                    .AddItem(
-                        new MenuItem(hero.ChampionName, "Disable on " + hero.ChampionName).SetValue(false));
-                Config.Item(hero.ChampionName).SetValue(false);
-            }
-            Config.AddToMainMenu();
-
-            Game.PrintChat("Exploit loaded!");
-            Game.OnGameUpdate += Game_OnGameUpdate;
-        }
-
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            foreach (var spell in Spells)
+            if (LXOrbwalker.CurrentMode.ToString() == "LaneClear" && !UnitUnderCursor().IsMe)
             {
-                if (spell.ChampionName == ObjectManager.Player.ChampionName)
+ 
+                if (Config.Item("UseQ").GetValue<bool>() )
                 {
-                    foreach (var hero in from hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => !hero.IsMe)
-                        let isEnabled = Config.Item(hero.ChampionName).GetValue<bool>()
-                        let championName = Config.Item(hero.ChampionName).Name
-                        where hero.ChampionName == championName & isEnabled && !hero.IsDead
-                        select hero)
-                    {
-                        Packet.C2S.Cast.Encoded(new Packet.C2S.Cast.Struct(hero.NetworkId, spell.AvailableSpell)).Send();
-                    }   
+                    Q.CastOnUnit(UnitUnderCursor(), true);
+ 
+                }
+                if (Config.Item("UseW").GetValue<bool>())
+                {
+                    W.CastOnUnit(UnitUnderCursor(), true);
+                }
+                if (Config.Item("UseE").GetValue<bool>())
+                {
+                    //E.CastOnUnit(UnitUnderCursor(), true);
+                }
+                if (Config.Item("UseR").GetValue<bool>())
+                {
+                    R.CastOnUnit(UnitUnderCursor(), true);
                 }
             }
         }
+ 
+        private static void OnGameLoad(EventArgs args)
+        {
+ 
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Config = new Menu("XcxooxL Sion", "Sion", true);
+            var tsMenu = new Menu("Target Selector", "Target Selector");
+            var orbwalkerMenu = new Menu("My Orbwalker", "my_Orbwalker");
+            LXOrbwalker.AddToMenu(orbwalkerMenu);
+            Config.AddSubMenu(orbwalkerMenu);
+ 
+            SimpleTs.AddToMenu(tsMenu);
+            Config.AddSubMenu(tsMenu);
+            Config.AddToMainMenu();
+            Config.AddSubMenu(new Menu("Combo Settings", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseQ", "Use Q").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseW", "Use W").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseE", "Use E").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseR", "Use R").SetValue(true));
+            Player = ObjectManager.Player;
+ 
+            Q = new Spell(SpellSlot.Q, 900);
+            W = new Spell(SpellSlot.W, 900);
+            E = new Spell(SpellSlot.E, 900);
+            R = new Spell(SpellSlot.R, 900);
+ 
+            Game.PrintChat("<font color='#F7A100'>Exploit .</font>");
+        }
+       
+ 
+ 
+        private static Obj_AI_Base UnitUnderCursor()
+        {
+            return ObjectManager
+                .Get<Obj_AI_Base>()
+                .Where(x => x.IsAlly || x.IsEnemy)
+                .FirstOrDefault(unit => Vector2.Distance(Game.CursorPos.To2D(), unit.ServerPosition.To2D()) < 200);
+        }
+ 
     }
 }
